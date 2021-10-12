c141400546e599633689e7b4a9248b4a3581d603
sunsookchung <79157199+sunsookchung@users.noreply.github.com>
2021-10-12 16:31:42 -0400

    Add files via upload

---
+++ protein_property.py
@@ -0,0 +1,9 @@
+def read_AA_tables(fname):
+  AA_abbr=dict()
+  with open(fname,'r') as f:
+    next(f) # skip the first line 
+    for aline in f.readlines():
+      #print(aline)
+      cols=aline.split(',')
+      AA_abbr[cols[1]]=cols[0]
+  return AA_abbr
